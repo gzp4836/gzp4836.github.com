8537a42b45919feb222838c29066fdb7941eeba5
ace <gzp4836@sina.com>
2020-03-30 09:31:14 +0800

    商户小二视频

---
+++ jd-code-count/js/videoData.js
@@ -0,0 +1,20 @@
+var data = [
+	{
+		keyWord: 'KA客户',
+		imgSrc: '//storage.jd.com/common.org/video/KA%E5%AE%A2%E6%88%B7%E5%BC%80%E5%8F%91%E6%94%BB%E7%95%A5.jpg',
+		videoSrc: [
+			'//storage.jd.com/common.org/video/KA%E5%AE%A2%E6%88%B7%E5%BC%80%E5%8F%91%E6%94%BB%E7%95%A5-1.mp4',
+			'//storage.jd.com/common.org/video/KA%E5%AE%A2%E6%88%B7%E5%BC%80%E5%8F%91%E6%94%BB%E7%95%A5-2.mp4',
+			'//storage.jd.com/common.org/video/KA%E5%AE%A2%E6%88%B7%E5%BC%80%E5%8F%91%E6%94%BB%E7%95%A5-3.mp4'
+		],
+		title: 'KA客户开发攻略',
+		name: '作者：张兴华'
+	},
+	{
+		keyWord: 'BD培训',
+		imgSrc: '//storage.jd.com/common.org/video/KA%E5%AE%A2%E6%88%B7%E5%BC%80%E5%8F%91%E6%94%BB%E7%95%A5.jpg',
+		videoSrc: ['//storage.jd.com/common.org/video/%E6%95%B0%E7%A7%91%E8%81%94%E7%9B%9F%E5%B0%8F%E7%A8%8B%E5%BA%8F0310.mp4'],
+		title: '数科联盟',
+		name: '作者：贾思思'
+	}
+]
